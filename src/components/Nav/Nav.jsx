b3860bc57929f82ed406b931697aa557fea60abe
Abu Raihan <137011693+RaihanSoft@users.noreply.github.com>
2024-09-03 16:47:45 +0600

    Navbar responsive

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Nav() {
     return (
         <div>
-            <nav className='flex justify-between px-20 shadow-xl py-2'>
+            <nav className='flex justify-between px-5 xl:px-20 shadow-xl py-2'>
                 <div className='flex items-center  gap-3'>
                     <div>
                         <img className='h-[30px]' src="./Frame.png" alt="" />
@@ -13,11 +13,17 @@ function Nav() {
                     </div>
                 </div>
 
-                <div className='space-x-5'>
+                <div className='space-x-5 hidden md:block'>
                     <a href="#">About Us</a>
                     <a href="#">Developers</a>
                     <button className='btn '>Follow Us on</button>
+
                 </div>
+
+                <div className='flex items-center justify-center text-xl md:hidden'>
+                    <i class="ri-bar-chart-horizontal-line"></i>
+                </div>
+
 
 
 
