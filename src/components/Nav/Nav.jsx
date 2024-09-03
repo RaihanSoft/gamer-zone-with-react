4d4929e51e3d745ca4ede6ac4623061ff6129465
Abu Raihan <137011693+RaihanSoft@users.noreply.github.com>
2024-09-03 16:42:15 +0600

    Navbar added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function Nav() {
     return (
-        <div>Nav</div>
+        <div>
+            <nav className='flex justify-between px-20 shadow-xl py-2'>
+                <div className='flex items-center  gap-3'>
+                    <div>
+                        <img className='h-[30px]' src="./Frame.png" alt="" />
+                    </div>
+                    <div>
+                        <h1 className='text-2xl font-bold' > <span className='text-[#FF42A5FF]'>G</span>amer</h1>
+                    </div>
+                </div>
+
+                <div className='space-x-5'>
+                    <a href="#">About Us</a>
+                    <a href="#">Developers</a>
+                    <button className='btn '>Follow Us on</button>
+                </div>
+
+
+
+            </nav>
+        </div>
     )
 }
 
