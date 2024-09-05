bf17bad208a0af75d5cc29495c7fda8a99fdf2ff
Rayhan Baee <aboraihan14@gmail.com>
2024-09-05 13:55:20 +0600

    Play section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from './components/Nav/Nav'
 import Header from './components/Header/Header'
 import Gamer from './components/Gamer/Gamer'
 import Online from './components/Online/Online'
+import Plays from './components/Plays/Plays'
 
 function App() {
  
@@ -15,6 +16,7 @@ function App() {
       <Header></Header>
       <Gamer></Gamer>
       <Online></Online>
+      <Plays></Plays>
 
     </>
   )
---
+++ src/components/Plays/Plays.jsx
@@ -0,0 +1,23 @@
+function Plays() {
+  return (
+    <div className="w-11/12 mx-auto mt-10 relative">
+      <section className="grid grid-cols-2">
+        <div className=" space-y-5 pt-32  text-center">
+          <h2 className="text-4xl font-bold"> <span className="text-[#F948B2]">Play Instantly,</span> <br /> No Downloads <br /> No Installs.</h2>
+          <p className="w-1/3 mx-auto">Play games online for free without downloading. Just click the Play button and play instantly in your browser.</p>
+        </div>
+
+        <div>
+          <img src="./Rectangle 55.png" alt="" />
+        </div>
+      </section>
+
+
+      <div className="absolute -bottom-16 right-[550px]">
+       <img src="./Rectangle 66.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Plays;
