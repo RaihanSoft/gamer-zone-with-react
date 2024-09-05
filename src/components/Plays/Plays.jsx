96290d1ccd18ab4a0a0693ad61f0da9a63e665d3
Rayhan Baee <aboraihan14@gmail.com>
2024-09-05 14:01:32 +0600

    play section responsive

--- src/components/Plays/Plays.jsx
+++ src/components/Plays/Plays.jsx
@@ -1,10 +1,10 @@
 function Plays() {
   return (
     <div className="w-11/12 mx-auto mt-10 relative">
-      <section className="grid grid-cols-2">
-        <div className=" space-y-5 pt-32  text-center">
-          <h2 className="text-4xl font-bold"> <span className="text-[#F948B2]">Play Instantly,</span> <br /> No Downloads <br /> No Installs.</h2>
-          <p className="w-1/3 mx-auto">Play games online for free without downloading. Just click the Play button and play instantly in your browser.</p>
+      <section className="grid grid-cols-1 md:grid-cols-2 justify-items-center gap-3">
+        <div className=" space-y-5 md:pt-32  text-center xl:w-2/3">
+          <h2 className="text-3xl md:text-4xl font-bold"> <span className="text-[#F948B2]">Play Instantly,</span> No Downloads  No Installs.</h2>
+          <p className="">Play games online for free without downloading. Just click the Play button and play instantly in your browser.</p>
         </div>
 
         <div>
@@ -13,7 +13,7 @@ function Plays() {
       </section>
 
 
-      <div className="absolute -bottom-16 right-[550px]">
+      <div className="absolute -bottom-16 right-[550px] hidden xl:flex">
        <img src="./Rectangle 66.png" alt="" />
       </div>
     </div>
